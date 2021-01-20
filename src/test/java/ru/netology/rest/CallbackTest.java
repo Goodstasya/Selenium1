a0d3b74e93f915e161b6841ccf27672cb53e586c
pavel8338 <pavel8338@gmail.com>
2021-01-20 18:50:10 +0300

    Second commit

--- src/test/java/ru/netology/rest/CallbackTest.java
+++ src/test/java/ru/netology/rest/CallbackTest.java
@@ -1,35 +1,22 @@
 package ru.netology.rest;
 
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class CallbackTest {
-    private WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll(){
-    System.setProperty("webdriver.chrome.driver","\\driver\\win\\chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp(){
-    driver=new ChromeDriver();
-    }
-
-    @AfterEach
-    void tearDown(){
-    driver.quit();
-    driver=null;
-    }
     @Test
-    void shouldTestSomething(){
-    throw new UnsupportedOperationException();
+    void shouldSubmitRequest(){
+        open("http://localhost:9999");
+        SelenideElement form= $("[data-test-id=callback-form]");
+        form.$("[data-test-id=name]input").setValue("Василий");
+        form.$("[data-test-id=phone]input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
     }
-
-   // publicinterfaceWebDriverextendsSearchContext{voidget(Stringurl);
-    //StringgetCurrentUrl();
-    //StringgetTitle();
-    //List<WebElement>findElements(Byby);
-    //WebElementfindElement(Byby);
-    //StringgetPageSource();
-    //voidclose();
-    //voidquit();
-
 }
