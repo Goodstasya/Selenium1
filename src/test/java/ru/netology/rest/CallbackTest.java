01a42ddde761d79e7a5b341293af343c08df5d44
pavel8338 <pavel8338@gmail.com>
2021-01-20 21:53:56 +0300

    Third commit

--- src/test/java/ru/netology/rest/CallbackTest.java
+++ src/test/java/ru/netology/rest/CallbackTest.java
@@ -1,7 +1,11 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,11 +16,18 @@ public class CallbackTest {
     @Test
     void shouldSubmitRequest(){
         open("http://localhost:9999");
-        SelenideElement form= $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name]input").setValue("Василий");
-        form.$("[data-test-id=phone]input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+
+        SelenideElement form= $("[data-test-id=name]");
+        form.$(".input__control").setValue("Василий");
+
+        form= $("[data-test-id=phone]");
+        form.$(".input__control").setValue("+79270000000");
+
+        form= $("[data-test-id=agreement]");
+        form.$(".checkbox__box").click();
+
+        $("[role=button]").click();
+
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
